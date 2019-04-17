020d84171e8aff3b03fc33f5fd019d09c2cf3316
Alexander Strogush <48963488+alexanderstogush@users.noreply.github.com>
2019-04-17 18:41:54 +0300

    Homework 8 : some mistakes fixed

--- src/mate/academy/hw08/FindMinMax/FindMinMax.java
+++ src/mate/academy/hw08/FindMinMax/FindMinMax.java
@@ -1,13 +1,18 @@
 package mate.academy.hw08.FindMinMax;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class FindMinMax {
-/*
+
     public static <T> void findMinMax(
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
 
-        // your implementation here
         List list = stream.sorted(order).collect(Collectors.toList());
         if (!list.isEmpty()) {
             minMaxConsumer.accept((T) list.get(0),(T) list.get(list.size() - 1));
@@ -15,5 +20,4 @@ public class FindMinMax {
             minMaxConsumer.accept(null, null);
         }
     }
- */
 }

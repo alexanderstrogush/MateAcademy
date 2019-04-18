5b8e3c5a244c35c4c0b7c63f48853d1955ad1f00
Alexander Strogush <48963488+alexanderstogush@users.noreply.github.com>
2019-04-18 13:59:21 +0300

    Homework 10 : made a decomposition

--- src/main/java/mate/academy/hw10/WordCounter.java
+++ src/main/java/mate/academy/hw10/WordCounter.java
@@ -6,26 +6,41 @@ import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordCounter {
 
     public static void main(String[] args) {
-        Map<String, Integer> words = new HashMap<>();
+        Optional<String> optInput = getInputData();
+        String inputData = "";
+        if (optInput.isPresent()) {
+            inputData = optInput.get();
+        }
+        dataProcessing(inputData);
+    }
+
+    private static Optional<String> getInputData() {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            Pattern pattern = Pattern.compile("\\b\\w+\\b", Pattern.UNICODE_CHARACTER_CLASS);
-            Matcher matcher = pattern.matcher(reader.readLine());
-            while (matcher.find()) {
-                String key = matcher.group().toLowerCase();
-                if (words.containsKey(key)) {
-                    words.put(key, words.get(key) + 1);
-                } else {
-                    words.put(key, 1);
-                }
-            }
+            return Optional.of(reader.readLine());
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Fatal error");
+        }
+        return Optional.empty();
+    }
+
+    private static void dataProcessing(String inputData) {
+        Map<String, Integer> words = new HashMap<>();
+        Pattern pattern = Pattern.compile("\\b\\w+\\b", Pattern.UNICODE_CHARACTER_CLASS);
+        Matcher matcher = pattern.matcher(inputData);
+        while (matcher.find()) {
+            String key = matcher.group().toLowerCase();
+            if (words.containsKey(key)) {
+                words.put(key, words.get(key) + 1);
+            } else {
+                words.put(key, 1);
+            }
         }
         words.keySet().stream()
                 .peek(String::toLowerCase)
